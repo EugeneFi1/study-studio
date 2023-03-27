838fd1ee643095264355888c3e8409206f2e88c1
EugeneFi1 <eugenefil.ua@gmail.com>
2023-03-27 21:45:56 +0300

    header component is added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
+  exports: [
+    HeaderComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
 
-
+const ANGULAR_MATERIAL = [
+  MatToolbarModule,
+  MatMenuModule,
+  MatButtonModule
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ANGULAR_MATERIAL
+  ],
+  exports: [
+    ANGULAR_MATERIAL
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
