e30fabd653b78da21b0ef16fd2088c6fc83bf055
EugeneFi1 <eugenefil.ua@gmail.com>
2023-07-23 13:53:00 +0300

    upd

--- src/app/core/settings/settings.component.ts
+++ src/app/core/settings/settings.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CodeEditorModule, CodeModel } from '@ngstack/code-editor';
 
 @Component({
   selector: 'ss-settings',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CodeEditorModule],
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.less']
 })
 export class SettingsComponent {
+  theme = 'vs-dark';
 
+  codeModel: CodeModel = {
+    language: 'json',
+    uri: 'main.json',
+    value: '{}'
+  };
+
+  options = {
+    contextmenu: true,
+    minimap: {
+      enabled: true
+    }
+  };
+
+  onCodeChanged(value: string) {
+    console.log('CODE', value);
+  }
 }
