3164c5c8d14e174b071c11fba33e27f8d35e07a5
EugeneFi1 <eugenefil.ua@gmail.com>
2023-07-26 17:44:45 +0300

    upd

--- src/app/core/settings/settings.component.ts
+++ src/app/core/settings/settings.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CodeEditorModule, CodeModel } from '@ngstack/code-editor';
+import { CodeEditorComponent, CodeEditorModule, CodeModel } from '@ngstack/code-editor';
 import { ConfigService } from 'src/app/services/config.service';
 import { Observable, map } from 'rxjs';
 
@@ -13,6 +13,8 @@ import { Observable, map } from 'rxjs';
 })
 export class SettingsComponent implements OnInit {
   public theme = 'vs-dark';
+
+  @ViewChild(CodeEditorComponent) public codeEditor: CodeEditorComponent | undefined;
 
   public defaultModel: CodeModel = {
     language: 'json',
@@ -33,6 +35,7 @@ export class SettingsComponent implements OnInit {
       )
     );
 
+  // https://microsoft.github.io/monaco-editor/docs.html#enums/editor.EditorOption.html#contextmenu
   public options = {
     contextmenu: true,
     minimap: {
